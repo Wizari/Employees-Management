09425685c9f30ccb7a18eee2a39e0ba10222e713
wizari <git301398>
2019-07-09 18:28:20 +0300

    Initial

---
+++ src/main/java/org/r78/employeesmanagement/EmployeesManagementApplication.java
@@ -0,0 +1,15 @@
+package org.r78.employeesmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesManagementApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(EmployeesManagementApplication.class, args);
+        System.out.println("Hello World!");
+    }
+
+}
