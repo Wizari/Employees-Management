8a6a41afeea9415a0436aed6e61f316969dd864d
wizari <git301398>
2019-07-10 02:58:36 +0300

    SEM-4 требуется что бы логи помимо консоли дублировались в файл.
    Когда размер файла превышает 1MB логи должны писаться в следующий файл
    
    Количество файлов должно быть не более 10, затем замый старый файл затирается, более свежим

--- src/main/java/org/r78/employeesmanagement/controllers/LoggingController.java
+++ src/main/java/org/r78/employeesmanagement/controllers/LoggingController.java
@@ -1,19 +1,21 @@
 package org.r78.employeesmanagement.controllers;
 
 
-import lombok.extern.log4j.Log4j2;
-import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 //@Slf4j
-@Component
+@RestController
+//@Component
 public class LoggingController {
 
     Logger logger = LoggerFactory.getLogger(LoggingController.class);
 
+    @RequestMapping("/")
     public String index() {
         logger.trace("A TRACE Message");
         logger.debug("A DEBUG Message");
@@ -21,8 +23,6 @@ public class LoggingController {
         logger.warn("A WARN Message");
         logger.error("An ERROR Message");
 //        log.info("An INFO Message");
-
-
 
 
         return "Howdy! Check out the Logs to see the output...";
